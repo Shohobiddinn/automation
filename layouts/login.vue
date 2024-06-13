374b96ff45916ec412b8a77ca79340e0261cdf8b
Shohobiddinn <shohobiddinibrohimov@gmail.com>
2024-06-13 15:31:59 +0500

    login

---
+++ layouts/login.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="bg-gradient-to-b from-gray-100 to-blue-900 dark:from-gray-900 dark:to-blue-900 h-[90vh]">
+        <div class="container mx-auto">
+            <NuxtPage />
+        </div>
+
+    </div>
+</template>
---
+++ pages/login/index.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+definePageMeta({
+    layout: 'login'
+})
+</script>
+
+<template>
+    <div>
+        <h1> title </h1>
+    </div>
+</template>
